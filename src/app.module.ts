9c1aa9a2a5fc3be576457fda97f3a8307a551548
melony <hjz1010@gmail.com>
2022-10-22 09:02:37 +0900

    [ADD] Posts API 초기세팅

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeORMconfig } from './configs/typeorm.config';
 import { UsersModule } from './users/users.module';
+import { PostsModule } from './posts/posts.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(typeORMconfig),
-    UsersModule,  
+    UsersModule,
+    PostsModule,  
   ],
 })
 export class AppModule {}
